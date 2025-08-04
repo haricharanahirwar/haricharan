0019984f4a1e3e78837233793a8aca944f922824
haricharanahirwar <ahirwarharicharan8719@gmail.com>
2025-08-04 12:21:33 +0530

    code update

--- UI/src/component/projectcomponent/Project.jsx
+++ UI/src/component/projectcomponent/Project.jsx
@@ -8,13 +8,22 @@ function Project(){
         <div className="main-project-container">
             
             <div className="main-shippingwar-myfirst-project">
-                <img src="public/assets/images/Shippingwar.jpg" alt="shippingwar" />
+                <img src="/assets/images/Shippingwar.jpg" alt="shippingwar" />
                 <h4>Shippingwar</h4>
                 <div className="main-button-div">
                     <button className="github-button"> <a target="_blank" href="https://github.com/haricharanahirwar/the-shipping-war">Github</a></button>
                     <button className="github-button"> <a target="_blank" href="https://the-shipping-war-oiw4.vercel.app/">Live</a></button>
                 </div>
             </div>
+            <div className="main-electrohub-mysecond-project">
+                <img src="assets\images\screen.jpg" alt="electrohub" />
+                <h4>electroHub</h4>
+                <div className="main-button-div">
+                    <button className="github-button"> <a target="_blank" href="https://github.com/haricharanahirwar/electrohub">Github</a></button>
+                    <button className="github-button"> <a target="_blank" href="https://electrohub-nu.vercel.app/">Live</a></button>
+                </div>
+            </div>
+
             
         </div>
         </>
