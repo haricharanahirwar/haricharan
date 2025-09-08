d929e7a2401f390a176eb84e1001a1ca69f382b8
haricharanahirwar <ahirwarharicharan8719@gmail.com>
2025-09-08 11:16:17 +0530

    code update

--- UI/src/component/projectcomponent/Project.jsx
+++ UI/src/component/projectcomponent/Project.jsx
@@ -11,16 +11,16 @@ function Project(){
                 <img src="/assets/images/Shippingwar.jpg" alt="shippingwar" />
                 <h4>Shippingwar</h4>
                 <div className="main-button-div">
-                    <button className="github-button"> <a target="_blank" href="https://github.com/haricharanahirwar/the-shipping-war">Github</a></button>
-                    <button className="github-button"> <a target="_blank" href="https://the-shipping-war-oiw4.vercel.app/">Live</a></button>
+                    <button className="github-button"> <a target="_blank" href="https://github.com/haricharanahirwar/the-shipping-war"><i class="fa-brands fa-github"></i> Github</a></button>
+                    <button className="github-button"> <a target="_blank" href="https://the-shipping-war-oiw4.vercel.app/"><i class="fa-solid fa-laptop"></i> Live</a></button>
                 </div>
             </div>
             <div className="main-electrohub-mysecond-project">
-                <img src="assets\images\screen.jpg" alt="electrohub" />
+                <img src="assets/images/Screenshot (7).png" alt="electrohub" />
                 <h4>electroHub</h4>
                 <div className="main-button-div">
-                    <button className="github-button"> <a target="_blank" href="https://github.com/haricharanahirwar/electrohub">Github</a></button>
-                    <button className="github-button"> <a target="_blank" href="https://electrohub-nu.vercel.app/">Live</a></button>
+                    <button className="github-button"> <a target="_blank" href="https://github.com/haricharanahirwar/electrohub"><i class="fa-brands fa-github"></i>Github</a></button>
+                    <button className="github-button"> <a target="_blank" href="https://electrohub-nu.vercel.app/"><i class="fa-solid fa-laptop"></i> Live</a></button>
                 </div>
             </div>
 
