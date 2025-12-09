599f95352de95d10cff0fccb3358fe24a797f514
haricharanahirwar <ahirwarharicharan8719@gmail.com>
2025-12-09 17:43:19 +0530

    code update

--- UI/src/component/slidercomponent/Slider.jsx
+++ UI/src/component/slidercomponent/Slider.jsx
@@ -6,6 +6,7 @@ function Slider() {
     <div className="container-fluid slider-container">
       <div className="row align-items-center h-100">
         <div className="col-md-6 text-md-start main-left-container">
+          <p>Hello World,</p>
           <h2 className="animated-heading">I am MERN Stack Developer</h2>
           <button className="btn btn-light mt-3">
             <Link to="/about" className="learn-more-link">Learn More</Link>
