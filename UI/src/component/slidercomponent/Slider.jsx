e56a94459196f5ad7d4574698b2b38af0925109f
haricharanahirwar <ahirwarharicharan8719@gmail.com>
2025-12-17 15:50:26 +0530

    codeupdate

--- UI/src/component/slidercomponent/Slider.jsx
+++ UI/src/component/slidercomponent/Slider.jsx
@@ -1,16 +1,33 @@
-import "./Slider.css";
+ import "./Slider.css";
 import { Link } from "react-router-dom";
 
 function Slider() {
   return (
     <div className="container-fluid slider-container">
       <div className="row align-items-center h-100">
+
         <div className="col-md-6 text-md-start main-left-container">
-          <p>Hello World,</p>
-          <h2 className="animated-heading">I am MERN Stack Developer</h2>
-          <button className="btn btn-light mt-3">
-            <Link to="/about" className="learn-more-link">Learn More</Link>
+
+          <p className="ltr ltr-1">Hello, I'm</p>
+
+          <h2 className="developer-name ltr ltr-2">
+            Haricharan Ahirwar
+          </h2>
+
+          <h2 className="ltr ltr-3">
+            MERN Stack Developer
+          </h2>
+
+          <p className="developer-desc ltr ltr-4">
+            I build modern, responsive & scalable web applications.
+          </p>
+
+          <button className="btn btn-light mt-3 ltr ltr-5">
+            <Link to="/about" className="learn-more-link">
+              Learn More
+            </Link>
           </button>
+
         </div>
 
         <div className="col-md-6 main-right-container">
@@ -20,6 +37,7 @@ function Slider() {
             className="slider-image"
           />
         </div>
+
       </div>
     </div>
   );
